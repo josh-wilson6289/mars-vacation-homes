5b6b260074fa2381deef80abff1dc54fa6528cb6
Josh Wilson <joshua.wilson6289@gmail.com>
2020-08-25 21:33:32 -0500

    cleaned up code to operate as a function

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
  
   var queryDate = moment().subtract(1, 'days');
- 
   var queryYear = moment(queryDate).format("YYYY");
   var queryMonth = moment(queryDate).format("MM");
   var queryDay = moment(queryDate).format("D");
   var rover = "curiosity";
   var camera = "fhaz";
-  
   var key = "l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc";
 
+  
+  function getCameraResponse() {
   var querySelector = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover + "/photos?earth_date=" + queryYear + "-" + queryMonth + "-" + queryDay + "&camera=" + camera + "&api_key=" + key;
   
-  $.ajax({
-    url: querySelector,
-    method: "GET"
-  }).then (function(response) {
-    console.log(response);
-  });
-
-
-
+    $.ajax({
+      url: querySelector,
+      method: "GET"
+    }).then (function(response) {
+      console.log(response);
+    });
+  }
+  
+  getCameraResponse();
 });
