903ad02b76effe895343ba117a6bbd4cb84d806e
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-08-25 21:45:23 -0500

    Adding new stuff to js file

--- script.js
+++ script.js
@@ -14,23 +14,27 @@
 $( document ).ready(function() {
     console.log( "Getting Weather for Mars!" );
     getForecast();
-});
-
-function getForecast() {
-
+ });
+  
+ let marsWeather = [];
+  
+ function getForecast() {
+    //Link to the Nasa API
     var NasaQueryURL = "https://api.nasa.gov/insight_weather/?api_key=l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc&feedtype=json&ver=1.0"
-    //"https: //api.nasa.gov/insight_weather/?api_key=l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc&feedtype=json&ver=1.0";
-    //"https://api.openweathermap.org/data/2.5/forecast?zip=77521,us" + APIKey;
-
+  
+    //Call the Ajax to get the weather data
     $.ajax({
       url: NasaQueryURL,
       method: "GET"
     }).then(function(response) {
-
+  
       console.log(response);
-
+     
+  
+  
     })
     .catch((error) => {
         console.log(error)
     });
-}
+ }
+ 
