42580be8a8938a493e6fa58defd996464e32b6bb
Shubhangi Mundhada <shubhimundhada@gmail.com>
2020-08-25 19:28:39 -0500

    Got the link from nasa working to get weather

--- script.js
+++ script.js
@@ -1,1 +1,36 @@
 //Adding in code to test- Shubhangi M
+
+//Josh Back End Code 
+
+
+
+
+
+
+
+
+
+// Shubhangi Back End Code 
+$( document ).ready(function() {
+    console.log( "Getting Weather for Mars!" );
+    getForecast();
+});
+
+function getForecast() {
+
+    var NasaQueryURL = "https://api.nasa.gov/insight_weather/?api_key=l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc&feedtype=json&ver=1.0"
+    //"https: //api.nasa.gov/insight_weather/?api_key=l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc&feedtype=json&ver=1.0";
+    //"https://api.openweathermap.org/data/2.5/forecast?zip=77521,us" + APIKey;
+
+    $.ajax({
+      url: NasaQueryURL,
+      method: "GET"
+    }).then(function(response) {
+
+      console.log(response);
+
+    })
+    .catch((error) => {
+        console.log(error)
+    });
+}
