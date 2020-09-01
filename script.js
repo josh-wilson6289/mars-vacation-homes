31e8d11e23fb022cef76bae7899642946031f503
Destanee Glenn <destanee.glenn@gmail.com>
2020-08-31 19:23:51 -0500

    adding files again after save

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
+//Rover Select 
 var curiosityEl = document.querySelector("#curiosity");
 var opportunityEl =  document.querySelector("#opportunity");
 var spiritEl = document.querySelector("#spirit");
+//Camera Select 
+var cameraSelectEl = document.querySelector("#cameraSelect");
+var fhazSelect = document.querySelector("#fhaz");
+var rhazSelect = document.querySelector("#rhaz");
+var navcamSelect = document.querySelector("#navcam");
+//Button Select 
+var submitBtnEl = document.querySelector("#submitBtn");
+var clearBtnEl = document.querySelector("#clearBtn");
+//Validation (must choose only 1 camera, must choose only 1 camera)
 
 
-function  validation() {
-    if(!curiosityEl  ===  true || !opportunityEl === ture || !spiritEl === true){
-        return "Please select one check box"
-    }
-}
+
+
+
+
