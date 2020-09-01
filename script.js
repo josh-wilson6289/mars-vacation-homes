487394d04ada3d18b45bde92094aa6c05fa7eded
Josh Wilson <joshua.wilson6289@gmail.com>
2020-09-01 16:39:38 -0500

    all working except for typeError bug on line 71

--- script.js
+++ script.js
@@ -1,21 +1,8 @@
 $(document).ready(function() {
 
-//Rover Select 
-var curiosity = document.querySelector("#curiosity");
-var opportunity =  document.querySelector("#opportunity");
-var spirit = document.querySelector("#spirit");
-//Camera Select 
-var cameraSelectEl = document.querySelector("#cameraSelect");
-var fhaz = document.querySelector("#fhaz");
-var rhaz = document.querySelector("#rhaz");
-var navcam = document.querySelector("#navcam");
-//Button Select 
-var submitBtn = document.querySelector("#submitBtn");
-var clearBtn = document.querySelector("#clearBtn");
-//Validation (must choose only 1 camera, must choose only 1 camera)
-
 // array to store user-checked rovers
 var roversArr = [];
+
 
 var key = "l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc";
 
@@ -23,82 +10,81 @@ var key = "l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc";
       // get rover id and assign it to name.  pass it to getLastDate
       // camera equals user input for #cameraSelect.  Pass camera to getLastDate
       //getLastDate(rover, camera);
+    
+    var camera = $("#cameraSelect option:selected").val();
 
-    if (curiosity.checked == false && opportunity.checked == false && spirit.checked == false) {
+    if (document.getElementById("curiosity") == false && document.getElementById("opportunity") == false && document.getElementById("spirit") == false) {
       alert("Please choose a rover!");
     }
-    if (curiosity.checked == true) {
+    if (document.getElementById("curiosity").checked == true) {
       var curiosityRover = {
-      "name": "Curiosity",
-      "camera": "", 
+      "name": "curiosity",
+      "camera": camera, 
       "maxDate": ""};
       roversArr.push(curiosityRover);
     }
-    if (opportunity.checked == true) {
+    if (document.getElementById("opportunity").checked == true) {
       var opportunityRover = {
-        "name": "Opportunity",
-        "camera": "", 
+        "name": "opportunity",
+        "camera": camera, 
         "maxDate": ""};
       roversArr.push(opportunityRover);
     }
-    if (spirit.checked == true) {
+    if (document.getElementById("spirit").checked == true) {
       var spiritRover = {
-        "name": "Spirit",
-        "camera": "", 
+        "name": "spirit",
+        "camera": camera, 
         "maxDate": ""};
       roversArr.push(spiritRover);
     }
-    // need to figure out how to assign camera here
     getLastDate(roversArr);
+  }
+  
+  function getLastDate(roversArr) { 
+    for (var i = 0; i < roversArr.length; i++) {
+      //   // if statements to determine the correct maxDate for user camera
+       
+      if (roversArr[i].name == "opportunity" && roversArr[i].camera == "navcam") {
+        roversArr[i].maxDate = "2018-05-16";
+      }
+      else if (roversArr[i].name == "opportunity" && roversArr[i].camera =="fhaz") {
+        roversArr[i].maxDate = "2018-06-04";
+      }
+      else if (roversArr[i].name == "opportunity" && roversArr[i].camera == "rhaz") {
+        roversArr[i].maxDate = "2018-05-17";
+      }
+      else if (roversArr[i].name == "spirit" && roversArr[i].camera == "navcam") {
+        roversArr[i].maxDate = "2010-02-26";
+      }
+      else if (roversArr[i].name == "spirit" && roversArr[i].camera == "fhaz") {
+        roversArr[i].maxDate = "2010-02-14";
+      }
+      else if (roversArr[i].name == "spirit" && roversArr[i].camera == "rhaz") {
+        roversArr[i].maxDate = "2010-02-09";
+      }
+      else {
+        $.ajax({
+        url: "https://api.nasa.gov/mars-photos/api/v1/manifests/curiosity?&api_key=" + key,
+        method: "GET"
+        }).then (function(response) {
+          console.log(response.photo_manifest.max_date);
+          roversArr[i].maxDate = response.photo_manifest.max_date;
+          
+        });
+      }   
     }
-  
-  function getLastDate(roversArr) {
-    var querySelector = "https://api.nasa.gov/mars-photos/api/v1/manifests/" + name + "?" + "&api_key=" + key;
-
-
-      //   for (var i = 0; i < roversArr.length; i++) {
-      //   //   // if statements to determine the correct maxDate for user camera
-      //     if (rover.name = curiosity) {
-      //       $.ajax({
-      //       url: querySelector,
-      //       method: "GET"
-      //      }).then (function(response) {
-      //        rover.maxDate = response.photo_manifest.max_date;
-      //      });   
-      //     }
-      //     else if (rover.name = opportinity || rover.camera = navcam) {
-      //       rover.maxDate = "2018-05-16";
-      //     }
-      //     else if (rover.name = opportunity || rover.camera = fhaz) {
-      //       rover.maxDate = "2018-06-04";
-      //     }
-      //     else if (rover.name = opportunity || rover.camera = rhaz) {
-      //       rover.maxDate = "2018-05-17";
-      //     }
-      //     else if (rover.name = spirit || rover.camera = navcam) {
-      //       rover.maxDate = "2010-02-26";
-      //     }
-      //     else if (rover.name = spirit || rover.camera = fhaz) {
-      //       rover.maxDate = "2010-02-14";
-      //     }
-      //     else {
-      //       rover.maxDate = "2010-02-09"
-      //     }
-      //   }
-      // }
-
-      // // pass rover array to displayPicture function
+      console.log(roversArr);
+      // pass rover array to displayPicture function
       // displayPicture(roversArr);
-   
+  }  
 
 
 
-  function displayPicture(rover) {
-  
-    var querySelector = "https://api.nasa.gov/mars-photos/api/v1/rovers/" + rover.name + "/photos?earth_date=" + rover.maxDate + "&camera=" + rover.camera + "&api_key=" + key;
-
+  function displayPicture(roversArr) {
+    for (var i = 0; i < roversArr.length; i++)
+     
       $.ajax({
-        url: querySelector,
+        url: "https://api.nasa.gov/mars-photos/api/v1/rovers/" + roversArr[i].name + "/photos?earth_date=" + roversArr[i].maxDate + "&camera=" + roversArr[i].camera + "&api_key=" + key,
         method: "GET"
       }).then (function(response) {
         //here's where we'll get the latest image from the given rover and camera
@@ -110,7 +96,15 @@ var key = "l7taWHMaSee1eSh38lm8sF83paMJIJ9KJQ1ehkuc";
     // submit button runs getInput function
     $("#submitBtn").click(function() {
       event.preventDefault();
+    // clears out any previous items in roversArr
+      roversArr = [];
       getInput();
+    });
+
+    // still need to get this working
+    $("#clearBtn").click(function(){
+      event.preventDefault;
+      console.log("click");
     });
 
 
